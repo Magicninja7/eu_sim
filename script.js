24354d9652a7cf6f3a2ab1c8be954de5050e6168
Magicninja7 <j.t.ptaszek@gmail.com>
2026-01-07 17:17:56 +0100

    fixes;

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ const questions = [
   buildQuestion('AI algorithms discriminate!', 'Force to redesign algorithm', [-15, 0, 10, 10], 'Ignore issue', [0, 0, -15, -5]),
   buildQuestion('Kim Kardashian launches eco-friendly perfume line!', "How did this shit get on my desk?!", [0, 0, 0, 0], 'Invest EU funds', [-5, 0, 0, 0]),
   buildQuestion('George Clooney gets French citizenship!', "Seriously… this is my problem now?", [0, 0, 0, 0], 'Celebrate with decree', [0, 0, 5, 0]),
-  buildQuestion('Taylor Swift writes a song!', 'Its shit', [0, 0, -5, 0], 'Its kindof good', [0, 0, 5, 0]),
-  buildQuestion('Elon Musk tweets about EU bureaucracy!', "Subpoena him", [0, 0, 0, 0], 'Fine one of his companies', [-5, 10, 0, 5]),
-  buildQuestion('Reality TV star becomes climate ambassador!', "Is this actually a thing?", [0, 0, 0, 0], 'Fund the campaign', [-5, 0, 5, 0]),
+  buildQuestion('Taylor Swift writes a song about inflation getting too high!', 'Im getting paid to little to deal with this', [5, 0, 0, 0], 'Its kindof good', [0, 0, 5, 0]),
+  buildQuestion('Elon Musk tweets about EU bureaucracy!', "Subpoena him", [0, 0, 0, 0], 'Fine his companies', [10, 10, 0, 0]),
+  buildQuestion('Reality TV star becomes climate ambassador!', "Is this actually a thing now?", [0, 0, 0, 0], 'Fund the campaign', [-5, 0, 5, 0]),
   buildQuestion('Celebrity chef opens a zero-carbon restaurant in Brussels!', "Why am I handling this?", [0, 0, 0, 0], 'How does that even work?', [0, 0, 0, 0]),
-  buildQuestion('Famous pop star proposes new holiday in EU!', "I have a better idea, declare his birthday a national holiday", [0, 0, 5, 0], 'Declare national holiday', [0, 0, 10, 0])
+  buildQuestion('Famous pop star proposes new holiday in EU!', "I have a better idea, declare his birthday a national holiday", [0, 0, 5, 0], 'Declare the holiday', [0, 0, 5, 0])
 ];
 
 const initialStats = STATS.reduce((acc, key) => ({ ...acc, [key]: 100 }), {});
